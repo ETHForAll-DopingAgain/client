faafd2c3c894c1ef1aa617d2092c0dbba0b5c527
Darshan Kumar <itsdarshankumar@gmail.com>
2023-02-19 18:46:52 +0530

    made changes

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -4,10 +4,13 @@ import StartStream from "../pages/startStream";
 import Upload from "../components/VideoInput";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 import { Videos, Sidebar } from "./";
-import { AuthProvider } from '@arcana/auth';
+import { AuthProvider } from "@arcana/auth";
 import { Loader } from "./";
-import {  Card, CardContent, CardMedia } from "@mui/material";
+import { ABI } from "./abi";
+import { Card, CardContent, CardMedia } from "@mui/material";
 import ReactPlayer from "react-player";
+import { providers, ethers } from "ethers";
+import * as PushAPI from "@pushprotocol/restapi";
 
 const UserProfile = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
@@ -18,8 +21,57 @@ const UserProfile = () => {
   const [address, setAddress] = useState(null);
   const [pub_key, setPub_key] = useState(null);
   const path = window.location.href.split("/")[3];
-  console.log("userprofile path =>", path)
+  console.log("userprofile path =>", path);
+   const pAPI = process.env.REACT_APP_pAPI;
+   const Pkey = `0x${pAPI}`;
+  const deleteNotifcation = async () => {
+    console.log("push called");
+    const signer = new ethers.Wallet(Pkey);
+    try {
+      const apiResponse = await PushAPI.payloads.sendNotification({
+        signer,
+        type: 3, // subset
+        identityType: 2, // direct payload
+        notification: {
+          title: `[SDK-TEST] notification TITLE:`,
+          body: `[sdk-test] notification BODY`,
+        },
+        payload: {
+          title: `Unsubscribed`,
+          body: `you deleted the video succesfully`,
+          cta: "",
+          img: "",
+        },
+        recipients: "eip155:5:0xD4819586cBB21B3A819100040163C56210021899", // recipients addresses
+        channel: "eip155:5:0x3487262703E3529E0c466319B2A51cDce413B8f5", // your channel address
+        env: "staging",
+      });
+      console.log("API repsonse: ", apiResponse);
+    } catch (err) {
+      console.error("Error: ", err);
+    }
+  };
 
+  const deleteVideo = async (uid) => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    console.log("Using account: ", accounts[0]);
+    const provider = new providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner(accounts[0]);
+    const subContract = new ethers.Contract(
+      process.env.REACT_APP_ADDRESS,
+      ABI,
+      provider
+    );
+    await subContract
+      .connect(signer)
+      .deleteData(uid)
+      .then((tx) => {
+        console.log("added on chain tx:", tx);
+      });
+    await deleteNotifcation();
+  };
 
   useEffect(() => {
     async function getUser() {
@@ -32,8 +84,8 @@ const UserProfile = () => {
       setAddress(user.address);
       setPub_key(user.publicKey);
     }
-    getUser()
-  }, [])
+    getUser();
+  }, []);
   let page;
   if (selectedCategory === "Live") {
   } else if (selectedCategory === "Start Stream") {
@@ -52,66 +104,90 @@ const UserProfile = () => {
     page = <Videos videos={videos} />;
   }
 
-     if(!pub_key) return <Loader />;
-      var x= JSON.parse(localStorage.getItem("videos")).IDs[1]
-      console.log(x.name);
+  if (!pub_key) return <Loader />;
+  var len = JSON.parse(localStorage.getItem("videos")).IDs.length;
+  var x = JSON.parse(localStorage.getItem("videos")).IDs[len - 1];
+  console.log(x.name);
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
       </Box>
       <Stack sx={{ marginLeft: "100px", marginTop: "140px" }}>
-        <Stack sx={{ flexDirection: { sx: "column", md: "row" }, alignItems: "center", marginBottom: "60px" }}>
-          <Avatar alt="Remy Sharp" src="/static/images/avatar.jpg" sx={{ width: 80, height: 80, bgcolor: "green", marginRight: "10px" }} />
+        <Stack
+          sx={{
+            flexDirection: { sx: "column", md: "row" },
+            alignItems: "center",
+            marginBottom: "60px",
+          }}
+        >
+          <Avatar
+            alt="Remy Sharp"
+            src="/static/images/avatar.jpg"
+            sx={{
+              width: 80,
+              height: 80,
+              bgcolor: "green",
+              marginRight: "10px",
+            }}
+          />
           <Stack spacing={1} sx={{ marginLeft: "8px" }}>
-            <Typography variant="h5" color="white">Name: {username} </Typography>
-            <Typography variant="h6" color="gray">E-mail: {email}</Typography>
-            <Stack sx={{ flexDirection: 'column' }} gap={2}>
-              <Typography variant="subtitle1" color="gray">Address: {address}</Typography>
+            <Typography variant="h5" color="white">
+              Name: {username}{" "}
+            </Typography>
+            <Typography variant="h6" color="gray">
+              E-mail: {email}
+            </Typography>
+            <Stack sx={{ flexDirection: "column" }} gap={2}>
+              <Typography variant="subtitle1" color="gray">
+                Address: {address}
+              </Typography>
               {/* <Typography variant="subtitle1" color="gray">||</Typography> */}
-              <Typography variant="subtitle1" color="gray">Public Key: {pub_key} </Typography>
+              <Typography variant="subtitle1" color="gray">
+                Public Key: {pub_key}{" "}
+              </Typography>
             </Stack>
           </Stack>
         </Stack>
-        <Typography variant="h3" color="white">{username}'s Videos</Typography>
+        <Typography variant="h3" color="white">
+          {username}'s Videos
+        </Typography>
         {/* {page} */}
       </Stack>
 
-<div className="userVid">
-      <Card
-    sx={{
-      width: { xs: "100%", sm: "357px", md: "318px" },
-      boxShadow: "none",
-      borderRadius: 5,
-    }}
-  >
-    <ReactPlayer
-      url={`https://gateway.lighthouse.storage/ipfs/${x.hash}`}
-      className="react-player-icon"
-    />
-    <CardContent sx={{ backgroundColor: "#1E1E1E", height: "50px" }}>
-      <Typography
-        variant="subtitle1"
-        fontWeight="bold"
-        color="#FFF"
-      >
-        {x.name}
-      </Typography>
-      <Typography variant="subtitle2" color="gray"></Typography>
-      </CardContent>
-  </Card>
-  <button
-              className="primary-btn"
-              onClick={async () => {
-                await unsubscribe(item.hash);
-              }}
-            >
-              Delete Video
-            </button>
-  </div>
-
+      <div className="userVid">
+        <Card
+          sx={{
+            width: { xs: "100%", sm: "357px", md: "318px" },
+            boxShadow: "none",
+            borderRadius: 5,
+          }}
+        >
+          <ReactPlayer
+            url={`https://gateway.lighthouse.storage/ipfs/${x.hash}`}
+            className="react-player-icon"
+          />
+          <CardContent sx={{ backgroundColor: "#1E1E1E", height: "50px" }}>
+            <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+              {x.name}
+            </Typography>
+            <Typography variant="subtitle2" color="gray"></Typography>
+          </CardContent>
+        </Card>
+        <button className="primary-btn" onClick={async () => { await deleteVideo(x.hash);}}>
+          Delete Video
+        </button>
+      </div>
     </Stack>
-
   );
 };
 
